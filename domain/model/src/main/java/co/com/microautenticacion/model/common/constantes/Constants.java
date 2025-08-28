1e698196e78cba80f63e5585a01584a16555a16c
Helly Meneses <hmeneses2704@hotmail.com>
2025-08-27 19:34:11 -0500

    Se agregan constantes

--- domain/model/src/main/java/co/com/microautenticacion/model/common/constantes/Constants.java
+++ domain/model/src/main/java/co/com/microautenticacion/model/common/constantes/Constants.java
@@ -1,4 +1,8 @@
 package co.com.microautenticacion.model.common.constantes;
 
 public class Constants {
+  public static final String VERIFY_EMAIL="Verificando existencia de email: {}";
+  public static final String SUCCESS_SAVE="Usuario guardado en base de dato con ID: {}";
+  public static final String ERROR_SAVE="Error al guardar usuario";
+
 }
--- infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/microautenticacion/r2dbc/user/adapter/CreateUser2dbcAdapter.java
+++ infrastructure/driven-adapters/r2dbc-postgresql/src/main/java/co/com/microautenticacion/r2dbc/user/adapter/CreateUser2dbcAdapter.java
@@ -1,15 +1,18 @@
 package co.com.microautenticacion.r2dbc.user.adapter;
 
+import co.com.microautenticacion.model.common.constantes.Constants;
 import co.com.microautenticacion.model.user.User;
 import co.com.microautenticacion.model.user.gateways.UserRepository;
 import co.com.microautenticacion.r2dbc.user.transformers.UserR2dbcTransformer;
 import co.com.microautenticacion.r2dbc.user.dao.UserR2dbcRepository;
 import co.com.microautenticacion.r2dbc.user.dto.UserDto;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class CreateUser2dbcAdapter implements UserRepository {
@@ -22,11 +25,14 @@ public class CreateUser2dbcAdapter implements UserRepository {
   public Mono<User> createUser(User user) {
     UserDto dto = userR2dbcTransformer.toDto(user);
     return userR2dbcRepository.save(dto)
+            .doOnSuccess(savedDto -> log.info(Constants.SUCCESS_SAVE, savedDto.getId()))
+            .doOnError(ex -> log.error(Constants.ERROR_SAVE, ex))
             .map(userR2dbcTransformer::toEntity)
             .as(transactionalOperator::transactional);
   }
 
   public Mono<Boolean> verifyEmail(String email) {
+    log.info(Constants.VERIFY_EMAIL, email);
     return userR2dbcRepository.existsUserDtoByEmail(email);
   }
 }
